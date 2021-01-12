2178bd5de2cd126fb9934715e8a4902a5aff6a4e
David Clark <plotinussmith@gmail.com>
2021-01-11 23:38:09 -0600

    Final revision of day 12 v2

--- src/day-12-v2.lisp
+++ src/day-12-v2.lisp
@@ -1,21 +1,19 @@
 (defpackage :day-12-v2
   (:use :cl)
   (:import-from :utils :read-day-file)
+  (:import-from :alexandria :define-constant)
   (:export #:part-1 #:part-2))
 
 (in-package :day-12-v2)
 
-(defparameter +directions+ '((N . (0 . 1)) (E . (1 . 0)) (S . (0 . -1)) (W . (-1 . 0))))
-(defparameter *position* '(0 . 0))
-(defparameter *facing* '(1 0))
-(defparameter *waypoint* '(10 . 1))
+(define-constant +directions+ '((N . (0 . 1)) (E . (1 . 0)) (S . (0 . -1)) (W . (-1 . 0))) :test #'equal)
 
 (defun load-instructions ()
   (flet ((decode (s) (cons (intern (string-upcase (subseq s 0 1)) :day-12-v2) (parse-integer (subseq s 1)))))
     (mapcar #'decode (read-day-file "12"))))
 
-(defun manhattan-distance ()
-  (+ (abs (car *position*)) (abs (cdr *position*))))
+(defun manhattan-distance (pos)
+  (+ (abs (car pos)) (abs (cdr pos))))
 
 (defun move (current dir delta)
   (cons (+ (car current) (* delta (car dir)))
@@ -24,29 +22,26 @@
 (defun rotate (cell delta)
   (if (zerop delta) cell (rotate (cons (cdr cell) (- (car cell))) (- delta 90))))
 
-(defun play-game (func)
-  (let ((*position* '(0 . 0))
-        (*facing* '(1 . 0))
-        (*waypoint* '(10 . 1)))
-    (dolist (i (load-instructions))
-      (destructuring-bind (pre . delta) i
-        (funcall func pre delta)))
-    (manhattan-distance)))
-
+(defun play-game (init-pos init-rot decision)
+  (let ((pos init-pos) (rot init-rot))
+    (loop for (i . delta) in (load-instructions)
+          do (setf (values pos rot) (funcall decision pos rot i delta)))
+    (manhattan-distance pos)))
+  
 (defun part-1 ()
-  (flet ((game-func (pre delta)
+  (flet ((game-func (pos rot pre delta)
            (ecase pre
-             ((N S E W) (setf *position* (move *position* (cdr (assoc pre +directions+)) delta)))
-             (F (setf *position* (move *position* *facing* delta)))
-             (R (setf *facing* (rotate *facing* delta)))
-             (L (setf *facing* (rotate *facing* (- 360 delta)))))))
-    (play-game #'game-func)))
+             ((N S E W) (values (move pos (cdr (assoc pre +directions+)) delta) rot))
+             (F (values (move pos rot delta) rot))
+             (R (values pos (rotate rot delta)))
+             (L (values pos (rotate rot (- 360 delta)))))))
+    (play-game (cons 0 0) (cons 1 0) #'game-func)))
 
 (defun part-2 ()
-  (flet ((game-func (pre delta)
+  (flet ((game-func (pos rot pre delta)
            (ecase pre
-             ((N S E W) (setf *waypoint* (move *waypoint* (cdr (assoc pre +directions+)) delta)))
-             (F (setf *position* (move *position* *waypoint* delta)))
-             (R (setf *waypoint* (rotate *waypoint* delta)))
-             (L (setf *waypoint* (rotate *waypoint* (- 360 delta)))))))
-    (play-game #'game-func)))
+             ((N S E W) (values pos (move rot (cdr (assoc pre +directions+)) delta)))
+             (F (values (move pos rot delta) rot))
+             (R (values pos (rotate rot delta)))
+             (L (values pos (rotate rot (- 360 delta)))))))
+    (play-game (cons 0 0) (cons 10 1) #'game-func)))
