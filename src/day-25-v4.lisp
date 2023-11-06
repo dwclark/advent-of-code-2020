e9bb69780f39824121596d5880aa51b12090ec30
David Clark <me@nobody.com>
2023-11-05 21:03:56 -0600

    OK, need to really learn the loop macro.
    It really can make complex looping constructs simple

--- src/day-25-v4.lisp
+++ src/day-25-v4.lisp
@@ -1,29 +1,16 @@
-(defpackage :day-25-v4
-  (:use :cl)
-  (:import-from :utils :read-day-file :split-blank-lines)
-  (:export #:part-1 #:part-2))
-
+(defpackage :day-25-v4 (:use :cl) (:export #:part-1 #:part-2))
 (in-package :day-25-v4)
 
 (defparameter *divisor* 20201227)
 (defparameter *subject-number* 7)
 
 (defun find-loop-size (start key)
-  (loop with val = 1
-	while (not (= val key))
-	for loop-size from 0
-	do (setf val (rem (* val start) *divisor*))
-	finally (return loop-size)))
+  (loop for val = 1 then (rem (* val start) *divisor*)
+	until (= key val) counting val))
 
 (defun find-key (loop-size key)
-  (loop with val = 1
-	for i from 0 to loop-size
-	do (setf val (rem (* val key) *divisor*))
-	finally (return val)))
+  (loop for val = 1 then (rem (* val key) *divisor*)
+	repeat loop-size finally (return val)))
 
 (defun part-1 ()
   (find-key (find-loop-size *subject-number* 14222596) 4057428))
-    
-					     
-	
-				   
